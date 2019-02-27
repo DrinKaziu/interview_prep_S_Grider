80eaf0e6940db897f5b0852e1eb3e83112128a62
SnowSurfer <drinkaziu@gmail.com>
2019-02-26 20:15:22 -0500

    steps

--- capitalize/index.js
+++ capitalize/index.js
@@ -7,7 +7,7 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-// function capitalize(str) {
+function capitalize(str) {
   let words = str.split(' ');
   let newStr = [];
 
